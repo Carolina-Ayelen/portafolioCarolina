a99079a4e7ab4978d0237eb1b683a815eea325fe
Carolina-Ayelen <70728509+Carolina-Ayelen@users.noreply.github.com>
2021-09-14 17:48:11 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -25,8 +25,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-    $mail->Username = 'carolinaayelencalvino@gmail.com';                     //SMTP username
-    $mail->Password = 'carolina2112';                               //SMTP password
+    $mail->Username = 'emial@gmail.com';                     //SMTP username
+    $mail->Password = '1234';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
